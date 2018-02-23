1c87a9ebb574b3dcda72b65698d723c36eb73b7c
Vincent Huang <vincent.huang@wework.com>
2018-02-23 01:08:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require_relative 'config/environment'
+require_relative 'models/tic_tac_toe'
 
 class Application < Sinatra::Base
+
+  get '/' do
+    @game = TicTacToe.new
+    erb :index
+  end
+
+
+  post '/' do
+    @game = TicTacToe.new
+    @game.turns(params)
+    if (@game.over?)
+      if (@game.won?)
+        @winner = @game.winner
+        erb :winner
+      else
+        erb :draw
+      end
+    else
+      erb :index
+    end
+  end
 end
