ac09bb80f2d9f37f5e826122dffeb28d48ace7b3
Vincent Huang <vincent.huang@wework.com>
2018-02-23 00:40:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,12 +4,11 @@ require_relative 'models/tic_tac_toe'
 class Application < Sinatra::Base
 
   get '/' do
-    @game = TicTacToe.new
     erb :index
   end
 
 
   post '/' do
-
+    @game = TicTacToe.new
   end
 end
